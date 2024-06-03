11d8badeab6e3c805035255cce272be14b8c15e2
PhanVu6 <135114319+PhanVu6@users.noreply.github.com>
2024-06-04 00:59:24 +0700

    Update App.java

--- ThiCKOOP2021/sortstrategy/App.java
+++ ThiCKOOP2021/sortstrategy/App.java
@@ -1,4 +1,4 @@
-package com.sortstrategy;
+package sortstrategy;
 
 public class App {
     public static void main(String[] args) {
@@ -13,5 +13,81 @@ public class App {
         // Before sorting: [5 4 3 2 1]
         // After sorting: [1 2 3 4 5]
         // Number of swap: 10
+
+        // BubbleSort
+        System.out.println("Using Bubble Sort Algorithm:");
+
+        int[] data = new int[(int) ((Math.random() * 10))];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = (int) (Math.random() * 10);
+        }
+
+        System.out.print("Before sorting:");
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+
+        SortStrategy sortStrategy = SortStrategy.getInstance();
+        BubbleSort bubbleSort = new BubbleSort();
+        sortStrategy.setSortee(bubbleSort);
+        int countSwap = sortStrategy.sort(data);
+
+        System.out.print("After sorting:");
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Number of swap:" + countSwap);
+
+        // SelectionSort
+        System.out.println("Using Selection Sort Algorithm:");
+        int[] data1 = new int[(int) (Math.random() * 10)];
+        for (int i = 0; i < data1.length; i++) {
+            data1[i] = (int) (Math.random() * 10);
+        }
+
+        System.out.print("Before sorting:");
+        for (int i = 0; i < data1.length; i++) {
+            System.out.print(data1[i] + " ");
+        }
+        System.out.println();
+
+        SortStrategy sortStrategy1 = SortStrategy.getInstance();
+        SelectionSort selectionSort = new SelectionSort();
+        sortStrategy1.setSortee(selectionSort);
+        int countSwap1 = sortStrategy1.sort(data1);
+
+        System.out.print("After sorting:");
+        for (int i = 0; i < data1.length; i++) {
+            System.out.print(data1[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Number of swap:" + countSwap1);
+
+        // InsertionSort
+        System.out.println("Using Insertion Sort Algorithm:");
+        int[] data2 = new int[(int) (Math.random() * 10)];
+        for (int i = 0; i < data2.length; i++) {
+            data2[i] = (int) (Math.random() * 10);
+        }
+
+        System.out.print("Before sorting:");
+        for (int i = 0; i < data2.length; i++) {
+            System.out.print(data2[i] + " ");
+        }
+        System.out.println();
+
+        SortStrategy sortStrategy2 = SortStrategy.getInstance();
+        InsertionSort insertionSort = new InsertionSort();
+        sortStrategy2.setSortee(insertionSort);
+        int countSwap2 = sortStrategy2.sort(data2);
+
+        System.out.print("After sorting:");
+        for (int i = 0; i < data2.length; i++) {
+            System.out.print(data2[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Number of swap:" + countSwap2);
     }
 }
