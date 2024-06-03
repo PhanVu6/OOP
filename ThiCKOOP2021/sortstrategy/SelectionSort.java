e7b317dcf9745da62f4c07c3c9aee0b7c81cb99d
PhanVu6 <135114319+PhanVu6@users.noreply.github.com>
2024-06-04 01:00:20 +0700

    Update SelectionSort.java

--- ThiCKOOP2021/sortstrategy/SelectionSort.java
+++ ThiCKOOP2021/sortstrategy/SelectionSort.java
@@ -1,8 +1,26 @@
-package com.sortstrategy;
+package sortstrategy;
 
 public class SelectionSort implements ISort {
     @Override
     public int sort(int[] data) {
-        /* TODO */
+        int countSwap = 0;
+        int n = data.length;
+        for (int i = 0; i < n - 1; i++) {
+            int max_indx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (data[j] > data[max_indx]) {
+                    max_indx = j;
+                }
+
+                if (max_indx != i) {
+                    int temp = data[max_indx];
+                    data[max_indx] = data[i];
+                    data[i] = temp;
+                    countSwap++;
+                }
+
+            }
+        }
+        return countSwap;
     }
 }
