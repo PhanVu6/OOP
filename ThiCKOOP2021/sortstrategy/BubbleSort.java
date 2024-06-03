3a3f5a5d1cea954e1f18442d38bf638b056fcb2e
PhanVu6 <135114319+PhanVu6@users.noreply.github.com>
2024-06-04 00:59:41 +0700

    Update BubbleSort.java

--- ThiCKOOP2021/sortstrategy/BubbleSort.java
+++ ThiCKOOP2021/sortstrategy/BubbleSort.java
@@ -1,8 +1,20 @@
-package com.sortstrategy;
+package sortstrategy;
 
 public class BubbleSort implements ISort {
     @Override
     public int sort(int[] data) {
-        /* TODO */
+        int countSwap = 0;
+        int temp = 0;
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[i] > data[j]) {
+                    temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                    countSwap++;
+                }
+            }
+        }
+        return countSwap;
     }
 }
