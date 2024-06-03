09dcddf8fdefa6f73b5f2cde1cd79f300359b6b5
PhanVu6 <135114319+PhanVu6@users.noreply.github.com>
2024-06-04 00:59:57 +0700

    Update InsertionSort.java

--- ThiCKOOP2021/sortstrategy/InsertionSort.java
+++ ThiCKOOP2021/sortstrategy/InsertionSort.java
@@ -1,8 +1,20 @@
-package com.sortstrategy;
+package sortstrategy;
 
 public class InsertionSort implements ISort {
     @Override
     public int sort(int[] data) {
-        /* TODO */
+        int countSwap = 0;
+        for (int i = 0; i < data.length; i++) {
+            int key = data[i];
+            int j = i - 1;
+
+            while (j >= 0 && data[j] > key) {
+                data[j + 1] = data[j];
+                j--;
+                countSwap++;
+            }
+            data[j + 1] = key;
+        }
+        return countSwap;
     }
 }
