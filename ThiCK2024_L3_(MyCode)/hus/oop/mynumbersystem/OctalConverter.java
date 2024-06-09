6faf90b8f9bba9ce291778fbe06480f19e4c4b00
PhanVu6 <135114319+PhanVu6@users.noreply.github.com>
2024-06-09 17:41:35 +0700

    Update OctalConverter.java

--- ThiCK2024_L3_(MyCode)/hus/oop/mynumbersystem/OctalConverter.java
+++ ThiCK2024_L3_(MyCode)/hus/oop/mynumbersystem/OctalConverter.java
@@ -2,21 +2,34 @@ package hus.oop.mynumbersystem;
 
 public class OctalConverter extends AbstractNumberSystemConverter {
     public OctalConverter(ANumber aNumber) {
-        /* TODO */
+        super(aNumber);
     }
 
     /**
      * Chuyển đổi một số được biểu diễn trong hệ cơ số 10
      * sang số được biểu diễn trong hệ cơ số 8.
+     * 
      * @param decimal
      * @return xâu ký tự biểu diễn số trong hệ cơ số 8.
      *
-     * Yêu cầu: sử dụng thuật toán Euclid để chuyển đổi,
-     * không sử dụng thư viện chuyển đổi số có sẵn của Java.
+     *         Yêu cầu: sử dụng thuật toán Euclid để chuyển đổi,
+     *         không sử dụng thư viện chuyển đổi số có sẵn của Java.
      */
     @Override
     public String decimalTo(String decimal) {
-        /* TODO */
+        if (decimal.equals("0")) {
+            return "0";
+        }
+
+        int valueOfDecimal = Integer.parseInt(decimal);
+        StringBuilder octal = new StringBuilder();
+        while (valueOfDecimal != 0) {
+            int remainder = valueOfDecimal % 8;
+            octal.append(remainder);
+            valueOfDecimal /= 8;
+        }
+
+        return octal.reverse().toString();
     }
 
     /**
@@ -25,7 +38,8 @@ public class OctalConverter extends AbstractNumberSystemConverter {
      */
     @Override
     public void update(ANumber number) {
-        /* TODO */
+        this.convertedNumber = decimalTo(this.convertedNumber);
+        display();
     }
 
     /**
@@ -33,6 +47,6 @@ public class OctalConverter extends AbstractNumberSystemConverter {
      */
     @Override
     public void display() {
-        /* TODO */
+        System.out.println(this.convertedNumber + "(8)");
     }
 }
